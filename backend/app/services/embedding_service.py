aa2041b61fca76bacee5f22a3dccfef42ae2c52c
MedOussamaBraiek <oussemabraiek@gmail.com>
2026-05-02 19:38:27 +0200

    Improve model loading

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -1,9 +1,15 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+    return model
 
 def embed_texts(texts: list[str]):
+    get_model()
     return model.encode(texts)
 
 def embed_query(query: str):
+    get_model()
     return model.encode([query])
