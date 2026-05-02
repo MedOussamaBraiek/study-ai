8605b6a307fcbc65b28a60d423a205800b00962c
MedOussamaBraiek <oussemabraiek@gmail.com>
2026-05-02 18:56:21 +0200

    added frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title = "StudyAI API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000", "https://quizzy-ai-frontend.vercel.app"],  
     allow_methods=["*"],
     allow_headers=["*"],
 )
