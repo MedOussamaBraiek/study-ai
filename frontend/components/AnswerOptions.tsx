06b06b65735b0ddf74997e6c14a25c96ec67aa91
MedOussamaBraiek <oussemabraiek@gmail.com>
2026-05-02 14:32:18 +0200

    structure cleaning

--- frontend/components/AnswerOptions.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AnswerOptions = () => {
-  return <div>AnswerOptions</div>;
-};
-
-export default AnswerOptions;
--- frontend/components/FeedbackBox.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FeedbackBox = () => {
-  return <div>FeedbackBox</div>;
-};
-
-export default FeedbackBox;
