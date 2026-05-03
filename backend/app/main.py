579a73b8c3d89ebe95625592535fe4a0ace57a5e
MedOussamaBraiek <oussemabraiek@gmail.com>
2026-05-03 10:34:10 +0200

    cors problem

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,8 @@ app = FastAPI(title = "StudyAI API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://quizzy--ai.vercel.app"],  
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://localhost:3000","https://quizzy--ai.vercel.app"],  
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
